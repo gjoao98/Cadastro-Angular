3574531a261c693648001fb11fb710bad578ee96
Matheus <56494951+OdMatheuS@users.noreply.github.com>
2022-06-20 17:01:27 -0300

    refactor: add OnDestroy & "shorthand" to fn createAccService

--- frontend/src/app/views/create-account/create-account.component.ts
+++ frontend/src/app/views/create-account/create-account.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CreateAccountService } from '../components/create-account.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { CreateAccountService } from '../components/create-account.service';
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.css']
 })
-export class CreateAccountComponent implements OnInit {
+export class CreateAccountComponent implements OnInit,OnDestroy {
 
   createForm: FormGroup = new FormGroup(
     {
@@ -19,14 +19,19 @@ export class CreateAccountComponent implements OnInit {
     }
   );
 
+  private subs:Subscription|undefined = undefined;
+
   constructor(
     private createAccountService: CreateAccountService,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  ngOnDestroy(): void {
+    this.subs?.unsubscribe();
   }
+  
 
   onSubmit(event: Event): void {
     event.preventDefault();
@@ -35,13 +40,16 @@ export class CreateAccountComponent implements OnInit {
       return;
     }
 
-    const account = {
-      name: this.createForm.get('name')!.value,
-      email: this.createForm.get('email')!.value,
-      password: this.createForm.get('password')!.value
-    };
+    /*
+    not necessary, use "form.value" 
+      const account = {
+        name: this.createForm.get('name')!.value,
+        email: this.createForm.get('email')!.value,
+        password: this.createForm.get('password')!.value
+     };
+    */
 
-    this.createAccountService.create(account).subscribe(() => {
+    this.subs = this.createAccountService.create(this.createForm.value).subscribe(() => {
       this.router.navigate(['/confirm'])
     })
   }
